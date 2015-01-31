2ce1b39836fa06c07b2a586bee86bd9cd2b5a0db
Oskar Wirén <oskar.wiren@citrix.com>
2015-01-30 21:44:57 -0800

    initial commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'swirre' do
+
+end
+
+target 'swirreTests' do
+   pod 'Quick', '~> 0.2'
+   pod 'Nimble', '~> 0.3'
+end
+
---
+++ swirre/swirre.h
@@ -0,0 +1,19 @@
+//
+//  swirre.h
+//  swirre
+//
+//  Created by Oskar Wirén on 30/01/15.
+//  Copyright (c) 2015 Citrix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swirre.
+FOUNDATION_EXPORT double swirreVersionNumber;
+
+//! Project version string for swirre.
+FOUNDATION_EXPORT const unsigned char swirreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swirre/PublicHeader.h>
+
+
